1135c7cfe546d90accadf20d9a4163a1daa1a7b9
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-15 15:58:03 +0100

    snake wipe

--- snake/main.lua
+++ snake/main.lua
@@ -1,24 +1,12 @@
-local socket = require "socket"
-local Game = require "Game"
-
-PIXEL_SIZE = 10
-SPEED = 0.05
-
 function love.load()
-  game = Game.new()
 end
 
 function love.update(dt)
-  game:update()
-  game.snake:update()
-  socket.sleep(SPEED)
 end
 
 function love.draw()
-  game:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
   if scancode == "escape" then love.event.quit() end
-  game.lastkey = scancode
 end
