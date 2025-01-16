3065fb5f34c12cef488f99cecd98d0fc4296c67c
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-16 13:46:05 +0100

    commit

--- snake/main.lua
+++ snake/main.lua
@@ -1,6 +1,4 @@
 local socket = require "socket"
-local Grid = require "Grid"
-local Snake = require "Snake"
 
 TILE_SIZE = 10
 GROWTH_FACTOR = 3
@@ -8,16 +6,13 @@ SPEED = 0.05
 
 function love.load()
   math.randomseed(socket.gettime() * 1000)
-
-  grid = Grid.new(TILE_SIZE)
 end
 
 function love.update(dt)
-  grid:update()
+  socket.sleep(SPEED)
 end
 
 function love.draw()
-  grid:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
