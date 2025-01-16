b182becf7d0ea6e6af5c2df7a9a9497629a7fa38
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-16 13:47:39 +0100

    commit

--- snake/main.lua
+++ snake/main.lua
@@ -4,6 +4,11 @@ TILE_SIZE = 10
 GROWTH_FACTOR = 3
 SPEED = 0.05
 
+local function terminate()
+  love.event.quit()
+  os.exit()
+end
+
 function love.load()
   math.randomseed(socket.gettime() * 1000)
 end
@@ -16,5 +21,5 @@ function love.draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  if scancode == "escape" then love.event.quit() end
+  if scancode == "escape" then terminate() end
 end
