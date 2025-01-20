75175598b9c129f7dab97691e48aa2a211984ab6
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-20 14:13:49 +0100

    commit

--- tetris/conf.lua
+++ tetris/conf.lua
@@ -1,3 +1,4 @@
 function love.conf(t)
   t.window.title = "Tetris"
+  t.window.width = 550
 end
--- tetris/main.lua
+++ tetris/main.lua
@@ -1,29 +1,43 @@
 _G.lg = love.graphics
 
+GRID_SIZE = {w = 10, h = 20}
+
 local socket = require "socket"
 local pieces = require "pieces"
 
 local display
+local font
+
 local grid = {}
+
 local current_piece = {}
 local next_piece = {}
+local held_piece = {}
+
+local level
+local score
 
 function love.load()
   math.randomseed(math.floor(socket.gettime()) * 1000)
 
-  display = lg.newCanvas(lg.getHeight() * 1/2, lg.getHeight())
-
-  for y = 1, display:getHeight() / pieces.PIXEL_SIZE do
+  for y = 1, GRID_SIZE.h do
     local row = {}
-    for x = 1, display:getWidth() / pieces.PIXEL_SIZE do
+    for x = 1, GRID_SIZE.w do
       table.insert(row, 0)
     end
     table.insert(grid, row)
   end
 
+  display = lg.newCanvas(GRID_SIZE.w * pieces.PIXEL_SIZE, GRID_SIZE.h * pieces.PIXEL_SIZE)
+  font = lg.newFont(24)
+  lg.setFont(font)
+
   current_piece.index = 1
   current_piece.state = 1
   current_piece.pos = {x = 0, y = 0}
+
+  level = 1
+  score = 0
 end
 
 function love.update(dt)
@@ -35,22 +49,27 @@ function love.draw()
   lg.setColor(.1,.1,.1)
   lg.rectangle("fill", 0, 0, display:getWidth(), display:getHeight())
 
+  lg.setColor(.2, .2, .2)
+  for y = 1, #grid do
+    for x = 1, #grid[y] do
+      pieces.drawPixel(x, y, true)
+    end
+  end
+
   lg.setColor(1, 1, 1)
   pieces.drawPiece(pieces.tetrominos[current_piece.index], current_piece.state, 0, 0)
 
-  local x = (lg.getWidth() / 2) - (display:getWidth() / 2)
-  local y = (lg.getHeight() / 2) - (display:getHeight() / 2)
   lg.setCanvas()
+
+  local x = lg.getWidth() - display:getWidth()
+  local y = (lg.getHeight() / 2) - (display:getHeight() / 2)
   lg.draw(display, x, y)
+
+  x = 10
+  y = 10
+  lg.print(string.format("Score: %d", score), x, y)
 end
 
-function love.keypressed(key, scancode, isrepeat)
+function love.keypressed(key, scancode)
   if scancode == "escape" then love.event.quit() end
-  if scancode == "return" then
-    if current_piece.state < #pieces.tetrominos[current_piece.index].states then current_piece.state = current_piece.state + 1
-    else
-      current_piece.state = 1
-      current_piece.index = current_piece.index < #pieces.tetrominos and current_piece.index + 1 or 1
-    end
-  end
 end
