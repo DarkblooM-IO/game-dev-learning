d8aa7aa93ed90bb937277a9608c56d910f1999d0
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-20 09:37:37 +0100

    commit

--- tetris/main.lua
+++ tetris/main.lua
@@ -2,28 +2,26 @@ _G.lg = love.graphics
 
 PIXEL_SIZE = 20
 
-require "Piece"
 local socket = require "socket"
 
 local display
-local current_piece
 
 function love.load()
   math.randomseed(math.floor(socket.gettime()) * 1000)
   display = lg.newCanvas(lg.getHeight() * 1/2, lg.getHeight())
-  current_piece = TETROMINOS[math.random(#TETROMINOS)]
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-  current_piece:draw(display)
+  lg.setCanvas(display)
 
-  lg.setCanvas()
+  -- draw game state
 
   local x = (lg.getWidth() / 2) - (display:getWidth() / 2)
   local y = (lg.getHeight() / 2) - (display:getHeight() / 2)
+  lg.setCanvas()
   lg.draw(display, x, y)
 end
 
