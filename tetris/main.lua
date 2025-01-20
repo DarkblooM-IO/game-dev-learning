b6f653495c1183893a1f5226e3f49c5e82723c94
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-20 10:53:20 +0100

    commit

--- tetris/main.lua
+++ tetris/main.lua
@@ -1,28 +1,36 @@
 _G.lg = love.graphics
 
 local socket = require "socket"
+local pieces = require "pieces"
 
 local display
+local current_piece
+local current_state
 
 function love.load()
   math.randomseed(math.floor(socket.gettime()) * 1000)
   display = lg.newCanvas(lg.getHeight() * 1/2, lg.getHeight())
+  current_piece = pieces.tetrominos[math.random(#pieces.tetrominos)]
+  current_state = math.random(#current_piece.states)
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-  lg.setCanvas(display)
-
-  -- draw game state
-
-  local x = (lg.getWidth() / 2) - (display:getWidth() / 2)
-  local y = (lg.getHeight() / 2) - (display:getHeight() / 2)
-  lg.setCanvas()
-  lg.draw(display, x, y)
+  pieces.drawPiece(current_piece, current_state, 20, 20)
+  -- lg.setCanvas(display)
+  --
+  -- local x = (lg.getWidth() / 2) - (display:getWidth() / 2)
+  -- local y = (lg.getHeight() / 2) - (display:getHeight() / 2)
+  -- lg.setCanvas()
+  -- lg.draw(display, x, y)
 end
 
 function love.keypressed(key, scancode, isrepeat)
   if scancode == "escape" then love.event.quit() end
+  if scancode == "return" then
+    current_piece = pieces.tetrominos[math.random(#pieces.tetrominos)]
+    current_state = math.random(#current_piece.states)
+  end
 end
--- tetris/pieces.lua
+++ tetris/pieces.lua
@@ -7,25 +7,25 @@ i.color = {0,255,255}
 i.states = {
   {
     {0,0,0,0},
-    {1,1,1,1}
+    {1,1,1,1},
     {0,0,0,0},
     {0,0,0,0}
   },
   {
     {0,0,1,0},
-    {0,0,1,0}
+    {0,0,1,0},
     {0,0,1,0},
     {0,0,1,0}
   },
   {
     {0,0,0,0},
     {0,0,0,0},
-    {1,1,1,1}
+    {1,1,1,1},
     {0,0,0,0}
   },
   {
     {0,1,0,0},
-    {0,1,0,0}
+    {0,1,0,0},
     {0,1,0,0},
     {0,1,0,0}
   },
@@ -65,20 +65,119 @@ l.states = {
     {0,0,0}
   },
   {
-    {0,0,0},
-    {0,0,0},
-    {0,0,0}
+    {0,1,0},
+    {0,1,0},
+    {0,1,1}
   },
   {
     {0,0,0},
-    {0,0,0},
-    {0,0,0}
+    {1,1,1},
+    {1,0,0}
   },
   {
-    {0,0,0},
-    {0,0,0},
-    {0,0,0}
+    {1,1,0},
+    {0,1,0},
+    {0,1,0}
   }
 }
+
+local o = {}
+o.color = {255,255,0}
+o.states = {
+  {
+    {1,1},
+    {1,1}
+  }
+}
+
+local s = {}
+s.color = {0,255,0}
+s.states = {
+  {
+    {0,1,1},
+    {1,1,0},
+    {0,0,0}
+  },
+  {
+    {0,1,0},
+    {0,1,1},
+    {0,0,1}
+  },
+  {
+    {0,0,0},
+    {0,1,1},
+    {1,1,0}
+  },
+  {
+    {1,0,0},
+    {1,1,0},
+    {0,1,0}
+  }
+}
+
+local t = {}
+t.color = {153,0,255}
+t.states = {
+  {
+    {0,1,0},
+    {1,1,1},
+    {0,0,0}
+  },
+  {
+    {0,1,0},
+    {0,1,1},
+    {0,1,0}
+  },
+  {
+    {0,0,0},
+    {1,1,1},
+    {0,1,0}
+  },
+  {
+    {0,1,0},
+    {1,1,0},
+    {0,1,0}
+  }
+}
+
+local z = {}
+z.color = {255,0,0}
+z.states = {
+  {
+    {1,1,0},
+    {0,1,1},
+    {0,0,0}
+  },
+  {
+    {0,0,1},
+    {0,1,1},
+    {0,1,0}
+  },
+  {
+    {0,0,0},
+    {1,1,0},
+    {0,1,1}
+  },
+  {
+    {0,1,0},
+    {1,1,0},
+    {1,0,0}
+  }
+}
+
+pieces.tetrominos = {i, j, l, o, s, t, z}
+
+pieces.drawPiece = function (piece, state, x, y)
+  for py = 1, #piece.states[state] do
+    for px = 1, #piece.states[state][py] do
+      if piece.states[state][py][px] == 1 then
+        lg.setColor(love.math.colorFromBytes(piece.color))
+        lg.rectangle("fill", x + (px * PIXEL_SIZE - PIXEL_SIZE), y + (py * PIXEL_SIZE - PIXEL_SIZE), PIXEL_SIZE, PIXEL_SIZE)
+        lg.setColor(0,0,0)
+        lg.rectangle("line", x + (px * PIXEL_SIZE - PIXEL_SIZE), y + (py * PIXEL_SIZE - PIXEL_SIZE), PIXEL_SIZE, PIXEL_SIZE)
+      end
+    end
+  end
+end
 
 return pieces
