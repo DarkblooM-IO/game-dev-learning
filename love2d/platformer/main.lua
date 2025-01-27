5801bcc14662d6402e9797b73bf1df17a7c9a82e
DarkblooM-IO <darkbloompro@gmail.com>
2025-01-27 21:01:54 +0100

    commit

--- love2d/platformer/main.lua
+++ love2d/platformer/main.lua
@@ -4,21 +4,25 @@ _G.lk = love.keyboard
 local ent = require "utils.entities"
 local tick = require "utils.tick"
 
-RUN_SPEED = 500
+RUN_SPEED = 10
 
 local player
 
 function love.load()
-  tick.framerate = 30
+  tick.framerate = 60
 
   local x = lg.getWidth() / 2
   local y = lg.getHeight() / 2
   player = ent.PhysicsEntity.new(x,y, 15,15)
-    player.movement = {0,0}
+  player.movement = {0,0}
 end
 
 function love.update(dt)
-  player.pos = player.pos + player.vel * dt
+  player.movement = {
+    lk.isDown("left") and not lk.isDown("right") and 1 or 0,
+    lk.isDown("right") and not lk.isDown("left") and 1 or 0
+  }
+  player.pos.x = player.pos.x + RUN_SPEED * (player.movement[2] - player.movement[1])
 end
 
 function love.draw()
